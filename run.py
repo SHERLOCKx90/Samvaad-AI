ebc1ad86ff5ebd0740b24fc0ba6e424081feaf85
darthchild <ekagranigam@gmail.com>
2024-03-15 23:29:53 +0530

    added flask entry point & minor FE changes

---
+++ run.py
@@ -0,0 +1,29 @@
+from json import load
+
+from server.app import app
+from server.backend import Backend_Api
+from server.website import Website
+
+if __name__ == '__main__':
+    config = load(open('config.json', 'r'))
+    site_config = config['site_config']
+
+    site = Website(app)
+    for route in site.routes:
+        app.add_url_rule(
+            route,
+            view_func=site.routes[route]['function'],
+            methods=site.routes[route]['methods'],
+        )
+
+    backend_api = Backend_Api(app, config)
+    for route in backend_api.routes:
+        app.add_url_rule(
+            route,
+            view_func=backend_api.routes[route]['function'],
+            methods=backend_api.routes[route]['methods'],
+        )
+
+    print(f"Running on port {site_config['port']}")
+    app.run(**site_config)
+    print(f"Closing port {site_config['port']}")
